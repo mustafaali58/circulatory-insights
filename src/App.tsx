976b700c5880c9775c6a59062baedc0ba6029188
mustafaali58 <136169312+mustafaali58@users.noreply.github.com>
2026-02-19 21:38:02 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
+        <Routes basename="/circulatory-insight">
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
