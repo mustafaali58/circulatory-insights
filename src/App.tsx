8193ea3baad6361f4f8b1253063e9dc1b990efcf
mustafaali58 <136169312+mustafaali58@users.noreply.github.com>
2026-02-19 21:43:33 +0300

    sa

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/circulatory-insight">
+      <BrowserRouter basename="/circulatory-insights">
         <Routes >
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
